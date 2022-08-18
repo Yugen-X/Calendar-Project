761abb12b3492ac898c61d72e6d05e8b86a28b97
Melvin <woozy912@gmail.com>
2022-08-18 14:57:55 -0400

    adds CST time zone to the app
    - added the time zone for the city of McKinney TX
    - added the day of the month in the app.js file aswell
    -- notes --
    - need to think about layout of the calandar
    - need to figure out how to display the dates

--- app.js
+++ app.js
@@ -2,8 +2,19 @@
 const monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
 const currentMonth = new Date();
 let getCurrentMonth = monthArr[currentMonth.getMonth()];
+console.log(getCurrentMonth)
 //get the current day of the month
 const dayArr = ['Sunday' ,'Monday', 'Tuesday', 'Wednsday', 'Thursday', 'Friday', 'Saturday'];
 const currentDay = new Date();
 let today = dayArr[currentDay.getDay()];
 console.log(today);
+//time zone conversion
+function calcTime(city, offset) {
+    d = new Date();
+    utc = d.getTime() + (d.getTimezoneOffset() * 60000);
+    nd = new Date(utc + (3600000*offset));
+    return `The local time in ${city} is ${nd.toLocaleString()}`;
+}
+let McKinneyTime = calcTime('McKinney', '-5');
+document.getElementById('current-month').innerHTML = getCurrentMonth;
+document.getElementById("mckinney-time").innerHTML = McKinneyTime;
